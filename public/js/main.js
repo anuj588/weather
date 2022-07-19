b7b93fdc32b3468ec9afe82b0ae3c7c9631edca3
Anuj sharma <72656672+anuj588@users.noreply.github.com>
2022-07-19 18:27:34 +0530

    Changes Made

--- public/js/main.js
+++ public/js/main.js
@@ -4,6 +4,19 @@ const city_name = document.getElementById("city_name")
 const temp_real_val = document.getElementById("temp_real_val")
 const temp_status = document.getElementById("temp_status")
 const datahide = document.querySelector(".middle_layer")
+
+const hum = document.getElementById("hum")
+const feels = document.getElementById("feels")
+const pressure = document.getElementById("pressure")
+ const speed = document.getElementById("speed")
+const gust = document.getElementById("gust")
+
+const weather = document.getElementById("weather")
+
+
+
+
+
 const getinfo = async (event) => {
     event.preventDefault()
 
@@ -12,19 +25,39 @@ const getinfo = async (event) => {
         city_name.innerText = `please enter the name before search`
         datahide.classList.add('data_hide')
     }
+
     else {
         try {
-            let url = `http://api.openweathermap.org/data/2.5/weather?q=${cityval}&units=metric&appid=2c392a09f77255da90dac0e4e4083dcc`
+            let url = `https://api.openweathermap.org/data/2.5/weather?q=${cityval}&units=metric&appid=2c392a09f77255da90dac0e4e4083dcc`
+           
             const response = await fetch(url)
+           
             const data = await response.json()
+            console.log(data)
             const arrdata = [data]
             city_name.innerText = `${arrdata[0].name},${arrdata[0].sys.country}`;
             temp_real_val.innerText = arrdata[0].main.temp;
-            //temp_status.innerText = arrdata[0].weather[0].main;
+            
+            feels.innerText = arrdata[0].main.feels_like;
+            weather.innerText = arrdata[0].weather[0].description;
+
+            hum.innerText = arrdata[0].main.humidity;
+            pressure.innerText = arrdata[0].main.pressure;
+            speed.innerText = arrdata[0].wind.speed;
+            gust.innerText = arrdata[0].wind.gust;
+       
+            
+
+
+
+
+
+
             const tempMood = arrdata[0].weather[0].main;
             if (tempMood == "Clear") {
                 temp_status.innerHTML =
                     "<i class='fas  fa-sun' style='color: #eccc68;'></i>";
+                    
             } else if (tempMood == "Clouds") {
                 temp_status.innerHTML =
                     "<i class='fas  fa-cloud' style='color: #f1f2f6;'></i>";
